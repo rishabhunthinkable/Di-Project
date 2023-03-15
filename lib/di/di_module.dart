e1aadaecac4d775fbd17cd57674f6b7d9c926c71
Rishabh Jain <rishabh.jain@unthinkable.co>
2023-03-15 17:36:14 +0530

    Updated the code for automated DI

--- lib/di/di_module.dart
+++ lib/di/di_module.dart
@@ -1,6 +1,5 @@
 import 'package:di_project/third_party_packages/google_auth.dart';
 import 'package:injectable/injectable.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../third_party_packages/firebase_auth.dart';
 @dev
@@ -13,8 +12,5 @@ abstract class DiModule{
   @dev
   @singleton
   Future<GoogleAuth> get googleAuth async => await GoogleAuth.instance();
-
-  @lazySingleton
-  Future<SharedPreferences> get sharedPref async => await SharedPreferences.getInstance();
-
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'package:di_project/blocs/auth_bloc.dart';
 import 'package:di_project/repository/auth_repo.dart';
 import 'package:di_project/repository/firebase_auth_repo.dart';
 import 'package:di_project/repository/google_auth_repo.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'di/di.dart';
 
 void main() async{
-  configureDependencies();
+  await configureDependencies();
   print('Main Started');
   AuthBloc bloc = getIt<AuthBloc>();
   bloc.setAuthenticationService(await getIt.getAsync<AuthRepo>(instanceName: const Named.from(GoogleAuthRepo).type.toString()));
@@ -23,8 +23,3 @@ void main() async{
 }
 
 
-@injectable
-class SharedPrefHelper{
-  late SharedPreferences preference;
-  SharedPrefHelper(this.preference);
-}
